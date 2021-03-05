4779f6ed68127674693668951c302aed38d6b02e
wizard <shaoyuanwei@weixing.com.cn>
2021-03-05 08:35:39 +0800

    uview+element

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus';
+import 'dayjs/locale/zh-cn'
+import locale from 'element-plus/lib/locale/lang/zh-cn'
+import 'element-plus/lib/theme-chalk/index.css';
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+const app = createApp(App)
+app.use(ElementPlus, { locale })
+app.mount('#app')
